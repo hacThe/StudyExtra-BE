416d712e904e4f3f027878487d0540eb05b96822
UIT19522006 <UIT19522006@gm.uit.edu.vn>
2022-03-17 00:46:31 +0700

    fix bug merge

--- routers/index.js
+++ routers/index.js
@@ -1,9 +1,10 @@
 const coursesRouter = require('./courses');
 const postsRouter = require('./posts');
-
+const documentController = require('./document');
 function route(app) {
     app.use("/api/posts", postsRouter);
     app.use("/api/courses", coursesRouter);
+    app.use("/api/document", documentController);
     app.use("/", (req, res) => res.send('hello'));
 }
 
