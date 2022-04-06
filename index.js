d685bbcc66e1f2040a2f3f1e376f317090dc073d
Lương Thiện Phước <19522055@gm.uit.edu.vn>
2022-04-06 23:38:39 +0700

    succes socket

--- index.js
+++ index.js
@@ -35,38 +35,46 @@ route(app);
 
 //Socket
 io.on('connection', (socket) => {
-    console.log('user connect' + socket.id);
+    console.log('user connect ' + socket.id);
     //Bắt sự kiện từ client
+    //chat tổng
     socket.on('on-chat', data => {
         console.log(data)
         io.emit('user-chat', data)
     })
-
+    //Chat riêng
     socket.on('send-to-user', data => {
         console.log(data);
-        io.emit(`user-${data.nameTo}`, data) 
-    })  
+        io.emit(`user-${data.nameTo}`, data)
+    })
+
+    // socket.on("sendDataClient", function (data) { // Handle khi có sự kiện tên là sendDataClient từ phía client
+    //     console.log(data)
+    //     socket.emit("sendDataServer", { data });// phát sự kiện  có tên sendDataServer cùng với dữ liệu tin nhắn từ phía server
+    // })
+
+    
     //Truyển ID socket về client
     socket.emit("me", socket.id)
 
-
     // Bắt sự kiện disconnect
-    socket.on('disconnect', () =>{
+    socket.on('disconnect', () => {
         console.log(socket.id + ' disconnect')
         socket.broadcast.emit("callEnded")
     })
 
-    
+
+
     //Gọi api
     axios.get(`https://jsonplaceholder.typicode.com/posts`)
-    .then(res => {
-        console.log("Thành công")
-    }).catch(err => {
-        console(err)
-    })
+        .then(res => {
+            console.log("Thành công")
+        }).catch(err => {
+            console(err)
+        })
 
     //
-    
+
 })
 
 //app listen
