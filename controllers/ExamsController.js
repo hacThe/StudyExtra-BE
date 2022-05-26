8030c5123516ff211fc90fd77a14eb72d8d2acca
TanThanh-CityBoi <tanthanhe@gmail.com>
2022-05-26 14:02:51 +0700

    add checking exam requirement api, post result exam api

--- controllers/ExamsController.js
+++ controllers/ExamsController.js
@@ -145,88 +145,100 @@ class ExamsController {
         var ObjectId = require('mongodb').ObjectId;
         const examID = new ObjectId(req.params.id);
         console.log("ID: ", examID);///
-        Exam.findOne({_id: examID}).exec()
-        .then((data)=>{
-            if(!data) return res.status(400).send("failure");
-            console.log(data)
-
-            return res.status(200).send(
-                JSON.stringify({
-                    data: data,
-                    message: "success"
-                })
-            )
-        })
-        .catch((err)=>{
-            res.status(400).send(err);
-        })
-    }
-
-    getQuestions = async (req, res) => {
-        var ObjectId = require('mongodb').ObjectId;
-        const questionsID = new ObjectId(req.body.questionsID);
-        console.log("qid: ", questionsID);
-        Questions.findOne({ _id: questionsID }).exec()
+        Exam.findOne({ _id: examID }).exec()
             .then((data) => {
-                console.log("data: ", data.ListQuestions);
-                res.status(200).send(
+                if (!data) return res.status(400).send("failure");
+                console.log(data)
+
+                return res.status(200).send(
                     JSON.stringify({
-                        data: data.ListQuestions
+                        data: data,
+                        message: "success"
                     })
                 )
             })
-            .catch((error) => {
-                console.log(error);
+            .catch((err) => {
+                res.status(400).send(err);
             })
     }
-    getResultExam = async (req, res) => {
-        const questionsID = req.body.questionsID;
-        const userAnswer = req.body.userAnswer;
-        const rightAnswer = [];
-        const examID = req.body.examID;
-        //  const username = res.locals.data.username;
-        const username = "tanthanh0805";
-        let score = 0;
+
+    checkExamRequirement = async (req, res) => {
+        const { id } = req.body;
+        const username = res.locals.data.username;
         let testRound = 1;
 
-        Questions.findOne({ _id: questionsID }).exec()
+        try {
+            var ObjectId = require('mongodb').ObjectId;
+            const examID = new ObjectId(id);
+            const exam = await Exam.findOne({ _id: examID }).exec();
+            console.log(exam);
+            if (!exam) return res.status(400).send("Exam not found");
+
+            const attention = await Attention.findOne({ username: username, examID: id }).sort({ '_id': -1 }).limit(1).exec();
+            if (attention !== null && attention.testRound == exam.testCount) return res.status(400).send(JSON.stringify({ message: "Bạn không còn lượt làm bài" }));
+            if (attention) {
+                testRound = attention.testRound + 1;
+            }
+
+            const newAttention = new Attention({
+                username: username,
+                examID: id,
+                score: 0,
+                testRound: testRound,
+                userAnswer: []
+            })
+            console.log("new attten: ", newAttention)
+            newAttention.save().then((data) => {
+                res.status(200).send(
+                    JSON.stringify({
+                        mmessage: "you can taking"
+                    })
+                )
+            });
+        }
+        catch (error) {
+            res.status(400).send(error);
+        }
+
+    }
+
+    postResultExam = async (req, res) => {
+        const { examID, userAnswer } = req.body;
+        const username = res.locals.data.username;
+        let rightAnswer = [];
+        let score = 0;
+        var ObjectId = require('mongodb').ObjectId;
+        const id = new ObjectId(examID)
+
+        const exam = await Exam.findOne({ _id: id }).exec();
+        console.log(exam);
+        if (!exam) res.status(400).send("Exam not found");
+
+        exam.listQuestion.forEach(element => {
+            rightAnswer.push(element.rightAnswer);
+        });
+        for (var i = 0; i < rightAnswer.length; i++) {
+            if (rightAnswer[i] + 1 === userAnswer[i]) score = score + 1;
+        }
+
+        Attention.findOne({ username: username, examID: examID }).sort({ '_id': -1 }).limit(1).exec()
             .then((data) => {
-                console.log("data: ", data.ListQuestions);
-                data.ListQuestions.forEach(element => {
-                    rightAnswer.push(element.rightAnswer);
-                });
-
-                for (var i = 0; i < rightAnswer.length; i++) {
-                    if (rightAnswer[i] == userAnswer[i]) score = score + 1;
-                }
-                Attention.findOne({ username: username, examID: examID }).sort({ '_id': -1 }).limit(1).exec()
-                    .then((data) => {
-                        if (data) testRound = data.testRound + 1;
-                        const newAttention = new Attention({
-                            username: username,
-                            examID: examID,
-                            score: score,
-                            testRound: testRound,
-                            userAnswer: userAnswer
+                if (!data) res.status(400).send('attention not found');
+                const attID = new ObjectId(data._id);
+                Attention.updateOne({ _id: attID }, {score: score, userAnswer: userAnswer}).exec()
+                .then(()=>{
+                    res.status(200).send(
+                        JSON.stringify({
+                            message: "Save result success"
                         })
-                        newAttention.save().then((data) => {
-                            console.log("attention: ", data);
-                            res.status(200).send(
-                                JSON.stringify({
-                                    rightAnswer: rightAnswer,
-                                    userAnswer: userAnswer,
-                                    score: score,
-                                })
-                            )
-                        });
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                    })
-
+                    )
+                })
+                .catch((error)=>{
+                    res.status(400).send(error);
+                })
             })
             .catch((error) => {
-                console.log(error);
+                res.status(400).send(error);
             })
     }
 }
--- routers/exam.js
+++ routers/exam.js
@@ -8,8 +8,8 @@ const ExamsController = require('../controllers/ExamsController');
 router.post('/delete-test-exam', ExamsController.deleteExam)
 router.post('/save-test-exam', ExamsController.saveExam)
 router.get('/edit/:id', ExamsController.getExamEdit)
-router.post('/getQuestions', AuthMiddleware, ExamsController.getQuestions);/////
-router.post('/getResultExam', ExamsController.getResultExam);
+router.post('/checkExamRequirement', AuthMiddleware, ExamsController.checkExamRequirement)
+router.post('/postResultExam', AuthMiddleware, ExamsController.postResultExam)
 router.get('/getAllTestExam', ExamsController.getAllTestExam)
 router.post('/addNewExam', ExamsController.addNewExam)
 router.get('/getExam/:id', ExamsController.getExam)
